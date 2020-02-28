f788982e1d9d111241feeb9bd06e5ae60354b6bb
vanesa-tamu <vahe5495@tamu.edu>
2020-02-28 10:35:10 -0600

    search form finished with correct data being logged in console

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterCard from "./CharacterCard";
 
-export default function SearchForm() {
- 
+export default function SearchForm({ characters}) {
+  const [search, setSearch] = useState('')
+  const [data, setData] = useState(characters)
+
+  const handleInput = (e) => {
+    setSearch(e.target.value)
+  }
+
+  useEffect(() => {
+    let output = characters.filter(char => 
+      char.name.toLowerCase().includes(search.toLowerCase())
+    )
+    setData(output)
+  }, [search, characters])
+
+  console.log('data!', data)
   return (
     <section className="search-form">
-     // Add a search form here
+     <form>
+       <label htmlFor='searching'>Search For:</label>
+         <input 
+            id='searching'
+            placeholder='character'
+            name='name'
+            onChange={handleInput}
+            value={search}
+            type='text'
+         />
+     </form>
     </section>
   );
 }
